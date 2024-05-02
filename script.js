18f81fb96e6b72bdd0984bd5c6c2a60be4600c0e
gertahoxha1 <hoxha_gerta14@hotmail.com>
2024-05-02 16:07:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  let user = getCookie("cookieAccepted");
+  if (user === ""){
+    document.getElementById("accept").style.display="block";
+  }
+
+  document.getElementById("accept").addEventListener("click", function(){
+    setCookie("cookieAccepted", "true", 365);
+    document.getElementById("accept").style.display= "none";
+})
+  
+
+
+  
